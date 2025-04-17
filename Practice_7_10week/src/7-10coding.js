ba84185ae1a554cb8633fb230bd2c5c3899e74a5
Lee-HaeSung <haesung116@naver.com>
2025-04-17 17:58:28 +0900

    practice_week7_4.17_1

--- Practice_7_10week/src/7-10coding.js
+++ Practice_7_10week/src/7-10coding.js
@@ -161,24 +161,62 @@
 //let CA = new CircleArea();
 //CA.CArea();
 
-//26번: 행성 문제2
-class Star {
-    transStar(){
-    this.starP = prompt("행성이름을 입력해주세요(한글).");
-    let planet = new Map([
-    ["수성", "Mercury"],
-    ["금성", "Venus"],
-    ["지구", "Earth"],
-    ["화성", "Mars"],
-    ["목성", "Jupiter"],
-    ["토성", "Saturn"],
-    ["천왕성", "Uranus"],
-    ["해왕성", "Neptune"]
-    ]);
-    console.log(planet.get(this.starP));
+////26번: 행성 문제2
+//class Star {
+//    transStar(){
+//    this.starP = prompt("행성이름을 입력해주세요(한글).");
+//    let planet = new Map([
+//    ["수성", "Mercury"],
+//    ["금성", "Venus"],
+//    ["지구", "Earth"],
+//    ["화성", "Mars"],
+//    ["목성", "Jupiter"],
+//    ["토성", "Saturn"],
+//    ["천왕성", "Uranus"],
+//    ["해왕성", "Neptune"]
+//    ]);
+//    console.log(planet.get(this.starP));
+//    }
+//}
+//let SP = new Star();
+//SP.transStar();
+
+//27번: 객체 만들기
+
+class Exam27 {
+    maps = new Map();
+
+    inputFunc() {
+        let names = prompt("이름을 공백으로 구분하여 입력하세요.");
+        let score = prompt("점수를 공백으로 구분하여 입력하세요.");
+        let arrName = names.split(" ");
+        let arrScore = score.split(" ");
+        if (arrName.length != arrScore.length) {
+            alert("두개 배열의 길이가 다릅니다. 다시 시작하세요.");
+            return false;   // 함수가 false 를 리턴한다.
+        }
+
+        for(let i = 0; i < arrName.length; i++){
+        this.maps.set(arrName[i], Number(arrScore[i])); // Number 는 문자열을 숫자로 변경
+        }
+        console.log(this.maps);
+        return true;    // 함수가 true 를 리턴한다.
+    }
+
+    outputFunc() {
+        let strOutput = "{";
+        for( let item of this.maps ) {
+            console.log(item);
+            strOutput += `'${item[0]}': ${item[1]}, `
+        }
+        strOutput = strOutput.substring(0 , strOutput.length - 2);
+        // 문자열의 길이에서 -2 시킨 길이의 문자열로 다시 저장. "abcd" => "ab"
+        strOutput += "}";
+        console.log(strOutput);
     }
 }
-let SP = new Star();
-SP.transStar();
 
-
+let exam = new Exam27();
+if ( exam.inputFunc() ) {   // inputFunc() 를 실행하고 리턴값이 true 이면
+    exam.outputFunc();  // outputFunc() 를 실행한다.
+}
