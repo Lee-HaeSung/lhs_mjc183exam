fd11693a2b7230ed33a2d1ea318ae904a2f570cc
Lee-HaeSung <haesung116@naver.com>
2025-05-08 19:33:41 +0900

    5.8_MyMathMathic

--- Study.Java/src/Main.java
+++ Study.Java/src/Main.java
@@ -1,48 +1,49 @@
+import com.mjc.studyjava.MyMathMathic;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//      System.out.println("Hello World !"); 헬로 월드 출력
-        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
-
-        System.out.println("3과_1번");
-        int x = 10;
-        int y = 20;
-        int z = (++x) + (y--);
-        System.out.println(z);
-
-        System.out.println("\n3과_2번");
-        int score = 85;
-        String result = (!(score>90)) ? "가" : "나";
-        System.out.println(result);
-
-        System.out.println("\n3과_3번");
-        int pencils = 534;
-        int student = 30;
-        int pencilsPerStudent = (pencils / student);
-        System.out.println("학생 한명이 가지는 연필 수는 " +  pencilsPerStudent);
-        int pencilsLeft = (pencils % student);
-        System.out.println("남은 연필 수는 " + pencilsLeft);
-
-        System.out.println("\n3과_4번");
-        int value = 356;
-        System.out.println(value/100*100);
-
-        System.out.println("\n3과_5번");
-        int lengthTop = 5;
-        int lengBottom = 10;
-        int height = 7;
-        double area = (double)((lengthTop+lengBottom) * height/2);
-        System.out.println(area);
-
-        System.out.println("\n3과_6번");
-        int x1 = 10;
-        int y1 = 5;
-
-        System.out.println((x1>7) && (y1<=5));
-        System.out.println((x1%3 == 2) || (y1%2 != 1));
-
+////      System.out.println("Hello World !"); 헬로 월드 출력
+//        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+//
+//        System.out.println("3과_1번");
+//        int x = 10;
+//        int y = 20;
+//        int z = (++x) + (y--);
+//        System.out.println(z);
+//
+//        System.out.println("\n3과_2번");
+//        int score = 85;
+//        String result = (!(score>90)) ? "가" : "나";
+//        System.out.println(result);
+//
+//        System.out.println("\n3과_3번");
+//        int pencils = 534;
+//        int student = 30;
+//        int pencilsPerStudent = (pencils / student);
+//        System.out.println("학생 한명이 가지는 연필 수는 " +  pencilsPerStudent);
+//        int pencilsLeft = (pencils % student);
+//        System.out.println("남은 연필 수는 " + pencilsLeft);
+//
+//        System.out.println("\n3과_4번");
+//        int value = 356;
+//        System.out.println(value/100*100);
+//
+//        System.out.println("\n3과_5번");
+//        int lengthTop = 5;
+//        int lengBottom = 10;
+//        int height = 7;
+//        double area = (double)((lengthTop+lengBottom) * height/2);
+//        System.out.println(area);
+//
+//        System.out.println("\n3과_6번");
+//        int x1 = 10;
+//        int y1 = 5;
+//
+//        System.out.println((x1>7) && (y1<=5));
+//        System.out.println((x1%3 == 2) || (y1%2 != 1));
+//
         System.out.println("\n3과_7번");
-
     }
 }
