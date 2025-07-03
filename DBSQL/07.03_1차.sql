9f9381b5b44ff2450e525f122447d0ecc6016f19
Lee-HaeSung <haesung116@naver.com>
2025-07-03 17:28:23 +0900

    07.03 DTSQL 1차

---
+++ DBSQL/07.03_1차.sql
@@ -0,0 +1,52 @@
+CREATE TABLE contact_tbl (
+	id INT UNSIGNED auto_increment NOT NULL,
+	name varchar(20) NOT NULL,
+	phoneNumber varchar(20) NOT NULL,
+	zipNumber varchar(5) NOT NULL,
+	email varchar(100) NOT NULL,
+	addr varchar(500) NOT NULL,
+	CONSTRAINT contact_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE saled_tbl (
+	id INT UNSIGNED auto_increment NOT NULL,
+	sale_dt DATE default(CURRENT_DATE) NOT NULL,
+	price INT UNSIGNED default(0) NOT NULL,
+	qty INT UNSIGNED default(0) NOT NULL,
+	product varchar(100) NOT NULL,
+	location_id INT UNSIGNED,
+	discount INT UNSIGNED default(0) NOT NULL,
+	contact_id INT UNSIGNED NOT NULL,
+	CONSTRAINT saled_pk PRIMARY KEY (id),
+	CONSTRAINT saled_fk FOREIGN KEY (location_id) REFERENCES location_tbl (id)
+	CONSTRAINT saled_fk FOREIGN KEY (contact_id) REFERENCES contact_tbl (id)
+);
+
+CREATE TABLE location_tbl (
+	id INT UNSIGNED auto_increment NOT NULL,
+	location varchar(500) NOT NULL,
+	CONSTRAINT location_pk PRIMARY KEY (id),
+	CONSTRAINT location_fk FOREIGN KEY (location) REFERENCES location_tbl (id)
+);
+
+CREATE TABLE learn_tbl (
+	id INT UNSIGNED auto_increment NOT NULL,
+	lecture_id INT UNSIGNED NOT NULL,
+	score DECIMAL(3,1) NOT NULL,
+	CONSTRAINT learn_pk PRIMARY KEY (id),
+);
+
+CREATE TABLE lecture_tbl (
+	id INT UNSIGNED auto_increment NOT NULL,
+	subject varchar(100) NOT NULL,
+	teacher varchar(20) NOT NULL,
+	root_num varchar(10) NOT NULL,
+	start_dt DATE NOT NULL,
+	end_dt DATE NOT NULL,
+	CONSTRAINT lecture_pk PRIMARY KEY (id),
+	CONSTRAINT lecture_fk FOREIGN KEY (subject) REFERENCES learn_tbl (lecture_id)
+);
+
+
+
+
