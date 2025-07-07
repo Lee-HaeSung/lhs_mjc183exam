85040c9745b0533ea9bac34e951d44faac9ecad6
Lee-HaeSung <haesung116@naver.com>
2025-07-07 17:15:20 +0900

    07_04) 문제최종

--- DBSQL/07.03_1차.sql
+++ DBSQL/07.03_1차.sql
@@ -18,22 +18,24 @@ CREATE TABLE saled_tbl (
 	discount INT UNSIGNED default(0) NOT NULL,
 	contact_id INT UNSIGNED NOT NULL,
 	CONSTRAINT saled_pk PRIMARY KEY (id),
-	CONSTRAINT saled_fk FOREIGN KEY (location_id) REFERENCES location_tbl (id)
-	CONSTRAINT saled_fk FOREIGN KEY (contact_id) REFERENCES contact_tbl (id)
+	CONSTRAINT saled_fk_loc FOREIGN KEY (location_id) REFERENCES location_tbl (id),
+	CONSTRAINT saled_fk_con FOREIGN KEY (contact_id) REFERENCES contact_tbl (id)
 );
 
 CREATE TABLE location_tbl (
 	id INT UNSIGNED auto_increment NOT NULL,
 	location varchar(500) NOT NULL,
-	CONSTRAINT location_pk PRIMARY KEY (id),
-	CONSTRAINT location_fk FOREIGN KEY (location) REFERENCES location_tbl (id)
+	CONSTRAINT location_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE learn_tbl (
 	id INT UNSIGNED auto_increment NOT NULL,
 	lecture_id INT UNSIGNED NOT NULL,
-	score DECIMAL(3,1) NOT NULL,
+	score DECIMAL(5,1) default(0) NOT NULL,
+	contact_id int UNSIGNED NOT NULL,
 	CONSTRAINT learn_pk PRIMARY KEY (id),
+	CONSTRAINT learn_fk FOREIGN KEY (lecture_id) REFERENCES lecture_tbl (id),
+	CONSTRAINT learn_fk_con FOREIGN KEY(contact_id) REFERENCES contact_tbl(id)
 );
 
 CREATE TABLE lecture_tbl (
@@ -43,8 +45,7 @@ CREATE TABLE lecture_tbl (
 	root_num varchar(10) NOT NULL,
 	start_dt DATE NOT NULL,
 	end_dt DATE NOT NULL,
-	CONSTRAINT lecture_pk PRIMARY KEY (id),
-	CONSTRAINT lecture_fk FOREIGN KEY (subject) REFERENCES learn_tbl (lecture_id)
+	CONSTRAINT lecture_pk PRIMARY KEY (id)
 );
 
 
