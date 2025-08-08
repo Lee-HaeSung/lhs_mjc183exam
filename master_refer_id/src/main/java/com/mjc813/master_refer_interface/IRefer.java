3cb9907a8434f90ed3abd642be009a69f7604c9b
Lee-HaeSung <haesung116@naver.com>
2025-08-08 12:30:47 +0900

    master_refer) 추가된 IRefer, ReferDto, ReferEnitiy를 간단하게 만들기

---
+++ master_refer_id/src/main/java/com/mjc813/master_refer_interface/IRefer.java
@@ -0,0 +1,20 @@
+package com.mjc813.master_refer_interface;
+
+import com.mjc813.master_refer_id.MasterTable;
+
+public interface IRefer {
+    Long getId();
+    void setId(Long id);
+
+    String getName();
+    void setName(String name);
+
+    Long getMasterTableId();
+    void setMasterTableId(Long masterTableId);
+
+    MasterTable getMaster();
+    void setMaster(MasterTable masterTableId);
+
+    default void copyMembers(IRefer src) {
+    }
+}
---
+++ master_refer_id/src/main/java/com/mjc813/master_refer_interface/ReferDto.java
@@ -0,0 +1,21 @@
+package com.mjc813.master_refer_interface;
+
+import com.mjc813.master_refer_id.MasterTable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReferDto implements IRefer {
+    private Long id;
+    private String name;
+    private Long masterTableId;
+    private MasterTable master;
+
+    public void copyMembers(ReferEntity entity) {
+    }
+}
