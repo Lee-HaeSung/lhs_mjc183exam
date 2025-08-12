06a5bd2a3d70bbac5873f2e8d47611463507a6b4
Lee-HaeSung <haesung116@naver.com>
2025-08-12 09:51:03 +0900

    springbootweb생성

---
+++ springbootwebprj/src/main/java/com/lhsspring/springbootwebprj/SpringbootwebprjApplication.java
@@ -0,0 +1,13 @@
+package com.lhsspring.springbootwebprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebprjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebprjApplication.class, args);
+    }
+
+}
---
+++ springbootwebprj/src/test/java/com/lhsspring/springbootwebprj/SpringbootwebprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhsspring.springbootwebprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebprjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
