492b15103aedd7b0d07ecb38b14e58b80e655a19
Lee-HaeSung <haesung116@naver.com>
2025-08-14 17:07:23 +0900

    springbootprj 삭제) 새로 만들 예정

--- springbootwebprj/src/main/java/com/lhsspring/springbootwebprj/genre/dto/GenreDto.java
+++
@@ -1,9 +0,0 @@
-package com.lhsspring.springbootwebprj.genre.dto;
-
-public class GenreDto {
-    private Long id;
-    /**
-     * 재즈 / 팝 / 힙합 / 락 / 해비메탈
-     */
-    private String name;
-}
--- springbootwebprj/src/main/java/com/lhsspring/springbootwebprj/song/service/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lhsspring.springbootwebprj.song.service;
-
-import com.lhsspring.springbootwebprj.song.dto.SongEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<SongEntity, Long> {
-}
